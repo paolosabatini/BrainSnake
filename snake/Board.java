8c1eedb5b27c18527aad38278d400faab0c19379
paolosabatini <paolosbtn@gmail.com>
2020-04-30 15:32:54 +0200

    Board and SnakeApp2D commented

--- snake/Board.java
+++ snake/Board.java
@@ -18,35 +18,56 @@ import java.awt.Rectangle;
 import java.awt.Font;
 import java.awt.FontMetrics;
 
+   
+/* \brief Class containing the code running the application.
+ *
+ * The board class manages the execution of the whole application. Therefore every object shown and running are members of this class.
+ * It extends the JPanel class and implements the ActionLister to listen key pressing.
+ */
 
 public class Board extends JPanel implements ActionListener {
     
-    private Snake snake;
-    private Apple apple;
-    private Grid grid;
-    private Timer timer;
+    private Snake snake; /*!< Instance of the snake */
+    private Apple apple; /*!< Instance of the apple */
+    private Grid grid;  /*!< Instance of the grid */
+    private Timer timer; /*!< Instance of the timer */
     
-    protected int IAPPLE_X = 200;
-    protected int IAPPLE_Y = 200;
-    protected int ISNAKE_X = 300;
-    protected int ISNAKE_Y = 200;
+    protected int IAPPLE_X = 200; /*!< Initial horizontal position of the apple */
+    protected int IAPPLE_Y = 200; /*!< Initial vertical position of the apple */
+    protected int ISNAKE_X = 300; /*!< Initial horizontal position of the snake */
+    protected int ISNAKE_Y = 200; /*!< Initial vertical position of the snake */
         
 
-    protected int B_WIDTH = 400;
-    protected int B_HEIGHT = 400;
-    protected int TOTAL_WIDTH = (int)(400*1.25);
-    protected int GRID_N = 20;
-    protected int GRID_WIDTH = B_WIDTH / GRID_N;
-    protected int GRID_HEIGHT = B_HEIGHT / GRID_N;
+    protected int B_WIDTH = 400; /*!< Width of the playing field */
+    protected int B_HEIGHT = 400; /*!< Height of the playing field and of the board */
+    protected int TOTAL_WIDTH = (int)(400*1.25); /*!< Total width of the board */
+    protected int GRID_N = 20; /*!< Number of segmentations */
+    protected int GRID_WIDTH = B_WIDTH / GRID_N; /*!< Horizontal segmentation size of the playground */
+    protected int GRID_HEIGHT = B_HEIGHT / GRID_N; /*!< Vertical segmentation size of the playground */
 
-    protected boolean inGame;
-    protected int Score;
-    protected int DELAY = 150;
-    protected static boolean VERBOSE = false;
+    protected boolean inGame; /*!<  Flag for "still in game"*/
+    protected int Score; /*!<  Variable containing the total score*/
+    protected int DELAY = 150; /*!<  Timer period (ms). The higher the slower the snake moves.*/
+    protected static boolean VERBOSE = false; /*!<  Flag to enable the verbose output */
+
+    /* \brief Constructor.
+     *
+     * Constructor of the class.
+     */
     
     public Board () {
 	initBoard ();
     }
+    
+    /* \brief Initialisation of the board.
+     *
+     * It sets up the initial conditions of the game:
+     * - Color and size of the board.
+     * - Initialisation of the grid instance (to deal with the segmentation).
+     * - Initialise the apple and the snake.
+     * - Initialise the inGame flag and the score.
+     * - It starts the timer, that makes the actions go.
+     */
 
     public void initBoard (){
 
@@ -71,12 +92,18 @@ public class Board extends JPanel implements ActionListener {
         timer.start();
     }
     
-    
-
+     /* \brief Action that is performed at each clock occurrence.
+     *
+     * It makes the snake move according to the key pressed, checks whether the apple is eaten (in case reproduce the apple).
+     * It checks collisions of the snake with borders or itself, update the score and the inGame flag.
+     * In case the game is over, the timer is stopped.
+     * The already pressed flag tells whether the action is already busy.
+     * 
+     * @params e Action event that is called at each timer clock.
+     */
     
     @Override
     public void actionPerformed(ActionEvent e) {
-
 	
 	if (VERBOSE)
 	    System.out.println ("printed in action");
@@ -93,18 +120,27 @@ public class Board extends JPanel implements ActionListener {
 	snake.ALREADY_PRESSED=false;
     }
 
+    /* \brief Makes the snake move.
+     *
+     * The snake is moved according to the key pressed. 
+     */
+
     public void updateSnake (){
 	snake.move();
     }
 
+    /* \brief Checks whether the apple is eaten.
+     *
+     * In case the head of the snake intersects the apple, another apple is created.
+     * The new apple must not be in the snake body. If it is randomly extracted in there, it is extracted again.
+     */
+    
     public void checkEating (){
 
 	Rectangle snake_head = new Rectangle();
 	snake_head.setRect (snake.x, snake.y, grid.getGridSizeX(), grid.getGridSizeY() );
 	Rectangle ap_rect = new Rectangle();
 	ap_rect.setRect (apple.x, apple.y, grid.getGridSizeX(), grid.getGridSizeY() );
-
-       
 	
 	if (snake_head.intersects (ap_rect)){
 	    
@@ -124,13 +160,17 @@ public class Board extends JPanel implements ActionListener {
 		}
 		if ( !snake.isIn( ap_rect ) ) break;
 	    }
-
 	    Score += 1;
 	}
     }
 
-    public boolean checkCollisions (){
+    /* \brief Checks the snake collides with borders or itself.
+     *
+     * It checks whether the snake head intersects the other snake blocks.
+     * It checks wehter the snake is fully contained by the borders.
+     */
 
+    public boolean checkCollisions (){
 	
 	// Collision against the snake
 	Rectangle snake_head = new Rectangle();
@@ -144,6 +184,14 @@ public class Board extends JPanel implements ActionListener {
 	return true;	
     }
     
+    /* \brief It reimplements the paintComponent method of JPanel.
+     *        This is extended to draw also the other objects
+     *        
+     * If the game is going all the objects are drawn with the score as well.
+     * If not, "Game Over" is displayed.
+     *
+     * @params g Graphics to paint the different objects. 
+     */
     
     @Override
     public void paintComponent(Graphics g) {
@@ -154,6 +202,7 @@ public class Board extends JPanel implements ActionListener {
 
 	    drawScore ((Graphics2D)g);
 	}
+
 	else {
 	    drawObjects (g);
 
@@ -165,6 +214,12 @@ public class Board extends JPanel implements ActionListener {
 	Toolkit.getDefaultToolkit().sync();
     }
 
+    /* \brief It draws all the objects needed to the code to run.
+     *        
+     * It draws borders, the apple and the snake.
+     *
+     * @params g Graphics to paint the different objects. 
+     */
 
     public void drawObjects (Graphics g){
 
@@ -173,16 +228,21 @@ public class Board extends JPanel implements ActionListener {
 	apple.drawApple ((Graphics2D)g);
 
 	snake.drawSnake ((Graphics2D)g);
-		
-	
     }
 
+    /* \brief It draws the score pad on top-right.
+     *        
+     * It creates the top-right box and draw the score info. 
+     *
+     * @params g2d Graphics2D to paint the different objects. 
+     */
+
     public void drawScore (Graphics2D g2d){
-	// g.setColor(Color.BLACK);
-        // g.drawString("Score: " + Score, 5, 15);
 	
 	double pad_offset = 0.02;
 	double pad_height_fraction = 0.4;
+
+	// It creates the box
 	g2d.setColor (Color.darkGray);
 	g2d.setStroke(new BasicStroke(2));
 	g2d.drawRect ((int) (B_WIDTH*(1+pad_offset) ) ,
@@ -190,6 +250,7 @@ public class Board extends JPanel implements ActionListener {
 		      (int) ((TOTAL_WIDTH-B_WIDTH)-1.5*pad_offset*B_WIDTH),
 		      (int) (B_HEIGHT*(pad_height_fraction)) );
 	
+	// It creates the "score"
 	g2d.setColor(Color.BLACK);
         Font normal = new Font("Helvetica", Font.BOLD, 14);
 	g2d.setFont (normal);
@@ -197,16 +258,21 @@ public class Board extends JPanel implements ActionListener {
 		       (int) (B_WIDTH+0.25*(TOTAL_WIDTH-B_WIDTH)),
 		       (int) (2.5*B_HEIGHT*pad_offset) );
 
-	
+	// It creates the score number	
 	g2d.setColor(Color.RED);
         Font large = new Font("Helvetica", Font.BOLD, 25);
 	g2d.setFont (large);
         g2d.drawString(""+(Score),
 		       (int) (B_WIDTH+0.4*(TOTAL_WIDTH-B_WIDTH)),
 		       (int) (0.5*B_HEIGHT*(pad_height_fraction)+ B_HEIGHT*(0.5*pad_offset) ) );
-
-
     }
+
+    /* \brief It draws "Game Over" at the end of the game.
+     *        
+     *  "Game Over" is displayed big at the center of the playground. 
+     *
+     * @params g Graphics to paint the different objects. 
+     */
 
     private void drawGameOver(Graphics g) {
 	
@@ -219,20 +285,40 @@ public class Board extends JPanel implements ActionListener {
         g.drawString(msg, (B_WIDTH - fm.stringWidth(msg)) / 2,
 		     B_HEIGHT / 2);
     }
-    
+
+    /* \brief Class needed to reimplement the KeyAdapter to the game needs.
+     *        
+     * It wants to extend the KeyAdapter class to adapt the snake movement.
+     */
 
     private class TAdapter extends KeyAdapter {
+
+	/* \brief Indicates what happens when a key is released.
+	 *        
+	 * It redirects to the snake action when the key is released.
+	 */
 
         @Override
         public void keyReleased(KeyEvent e) {
 	    snake.keyReleased(e);
         }
 
+	/* \brief Indicates what happens when a key is pressed.
+	 *        
+	 * It redirects to the snake action when the key is pressed.
+	 */
+
         @Override
         public void keyPressed(KeyEvent e) {
             snake.keyPressed(e);
         }
     }
+
+    
+    /* \brief Useful to propagate to the other classes whether the verbose is on/off.
+     *        
+     * Returns the VERBOSE member.
+     */
 
     public static boolean isVerbose() {return VERBOSE;}
 }
--- snake/SnakeApp2D.java
+++ snake/SnakeApp2D.java
@@ -3,12 +3,29 @@ package ps;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+/** \brief Main class of the application. 
+ *         It contains the main() function.
+ *
+ *  The main function instanciates the class. The constructor initialises a new Board, containing most of the code and set the execution exitation, title etc..
+ */
+
 public class SnakeApp2D extends JFrame {
-    
+
+
+    /** \brief Constructor.
+     *
+     *  Constructor of the class.
+     */
+
     public SnakeApp2D() {
 
         initUI();
     }
+    
+    /** \brief Initialisation of the UI.
+     *
+     *  It creates a new Board, set its size, title and makes the code exit at closing the board.
+     */
 
     private void initUI() {
 
@@ -21,7 +38,12 @@ public class SnakeApp2D extends JFrame {
 	setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }    
-    
+        
+    /** \brief Main function of the application.
+     *
+     *  It invokes the main function and the queue of events ot be exectued. It creates the SnakeApp2D.
+     */
+
     public static void main(String[] args) {
         
         EventQueue.invokeLater(() -> {
