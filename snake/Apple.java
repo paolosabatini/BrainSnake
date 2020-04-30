1cd964256bc705fb3d1740ee8173bb800fc44491
paolosabatini <paolosbtn@gmail.com>
2020-04-30 17:29:30 +0200

    all files are now commented

--- snake/Apple.java
+++ snake/Apple.java
@@ -7,19 +7,31 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
 
+/*! \brief Class that extends the Sprite class to implement the features of the apple.
+ *
+ *  It implements only the constructor and the drawing options.
+ */
+
 public class Apple extends Sprite {
 
     
-    private Color appleColor = new Color (252, 40, 0);
-    Grid grid;
-
+    private Color appleColor = new Color (252, 40, 0); /*!< Color of the apple */
+    Grid grid; /*!< Reference to the grid where the apple lives.*/
+    
+    /*! \brief Constructor of the class.
+     *
+     *  Setting up the position and the grid.
+     */
     
     public Apple (int x, int y, Grid grid){
 	super(x,y);
 	this.grid = grid; 
     }
 
-
+    /*! \brief Draws th apple.
+     *
+     *  It is basically a red rectangle with the same dimension as the grid segmentation.
+     */
 
     public void drawApple (Graphics2D g2d){
 	g2d.setColor (appleColor);
@@ -31,6 +43,5 @@ public class Apple extends Sprite {
 			  grid.getGridSizeY(),
 			  round_h,
 			  round_r);
-	
     }
 }
